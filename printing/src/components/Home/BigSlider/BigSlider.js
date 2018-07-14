beb3bbee037c9a7fe4616539c10929ca792ca6ca
megasharks <tigran.accontech@gmail.com>
2018-07-14 17:45:18 +0400

    replaced the big slider

--- printing/src/components/Home/BigSlider/BigSlider.js
+++ printing/src/components/Home/BigSlider/BigSlider.js
@@ -1,126 +1,102 @@
-export default {
-    data() {
-        return {
-            itemsCounter: {},
-            currentSlide: {},
-            navElemContainerElements: {},
-            bannersSlider: {}
-        }
-    },
-    mounted() {
-        this.initHomeSlider();
-        this.initLogo();
-    },
-    methods: {
-        initHomeSlider() {
-            this.itemsCounter = $('#items-counter');
-            this.currentSlide = $('#current-slide');
-            this.navElemContainer = $(".nav-element-container");
-            this.navElemContainerElements;
-            this.bannersSlider = $('#banners-slider');
-            this.bannersSlider.owlCarousel({
-                loop: false,
-                smartSpeed: 250,
-                margin: 0,
-                //nav: true,
-                dots: true,
-                items: 1,
-                autoHeight: true,
-                mouseDrag: false,
-                animateOut: 'fadeOut',
-                animateIn: 'fadeIn',
-                //touchDrag:false,
-                dotsContainer: ".banner-dots",
-                onInitialized: (event) => {
-                    this.calculateSlideInfo(event);
-                    this.createElements(event.item.index, event.item.count);
-                },
-                onTranslated: (event) => {
-                    this.calculateSlideInfo(event);
-                    this.createElements(event.item.index);
-                }
-            });
-        },
-        calculateSlideInfo(event) {
-            this.itemsCounter.text(event.item.count < 10 ? '/0' + (event.item.count) : '/' + event.item.count);
-            this.currentSlide.text(event.item.index + 1 < 10 ? '0' + (event.item.index + 1) : event.item.index + 1);
-        },
-        createElements(currentIndex, slideCount) {
-            if (slideCount) {
-                for (var i = 0; i < slideCount - 1; i++) {
-                    this.navElemContainer.append("<span class='' data-nav=" + (i + 1) + "></span>");
-                }
-                this.navElemContainerElements = $(".nav-element-container span");
-            } else {
-                this.navElemContainerElements.each(function(index) {
-                    if (currentIndex != $(this).data('nav')) {
-                        $(this).removeClass('active-nav');
-                    } else {
-                        $(this).addClass('active-nav');
-                    }
-                });
-            }
-            this.navElemContainerElements.on('click', () => {
-                this.bannersSlider.trigger('to.owl.carousel', $(this).data('nav'));
-            }); 
-        },
+ export default {
+   data() {
+     return { 
+       currentSlide: 1,
+       slidesLength: '', 
+       swiperOption: {
+         slidesPerView: 1,
+         fadeEffect: {
+           crossFade: true
+         },
+         effect: 'fade',
+         autoplay: true,
+         pagination: {
+           el: '.big-slider-pagination',
+           clickable: true
+         }
+       }
+     }
+   },
+   mounted() {
+     this.setSlidesItems(1);  //set slide item info.
 
-        initLogo() {
-            this.canvas = document.getElementById("canvas");
-            this.anim_container = document.getElementById("animation_container");
-            this.dom_overlay_container = document.getElementById("dom_overlay_container");
-            this.handleComplete();
-        },
-        handleComplete() {
-            //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
-            this.exportRoot = new lib.Logo_animation();
-            this.stage = new createjs.Stage(this.canvas);
-            this.stage.addChild(this.exportRoot);
-            //Registers the "tick" event listener.
-            var fnStartAnimation = () => {
-                    createjs.Ticker.setFPS(lib.properties.fps);
-                    createjs.Ticker.addEventListener("tick", this.stage);
-                }
-                //Code to support hidpi screens and responsive scaling.
+     this.swiper.on('slideChange', () => {
+       //handle slide change.
+       let currentIndex = this.swiper.activeIndex + 1;
+       this.setSlidesItems(currentIndex);
+     });
+   
+     this.initLogo();
 
-            this.makeResponsive(true, 'both', false, 1);
-            fnStartAnimation();
-        },
-        makeResponsive(isResp, respDim, isScale, scaleType) {
-            var lastW, lastH, lastS = 1;
-            window.addEventListener('resize', this.resizeCanvas);
+   },
+   computed: {
+     swiper() {
+       return this.$refs.bigSlider.swiper
+     },
+   },
+   methods: {
+     setSlidesItems(currentIndex) {
+       let slidesLength = this.swiper.slides.length;
+       this.slidesLength = (currentIndex < 10) ? '/0' + slidesLength : '/' + slidesLength;
+       this.currentSlide = (currentIndex < 10) ? '0' + currentIndex : currentIndex;
+     }, 
+     // Logo animation related
+     initLogo() {
+       this.canvas = document.getElementById("canvas");
+       this.anim_container = document.getElementById("animation_container");
+       this.dom_overlay_container = document.getElementById("dom_overlay_container");
+       this.handleComplete();
+     },
+     handleComplete() {
+       //This function is always called, irrespective of the content. You can use the variable "stage" after it is created in token create_stage.
+       this.exportRoot = new lib.Logo_animation();
+       this.stage = new createjs.Stage(this.canvas);
+       this.stage.addChild(this.exportRoot);
+       //Registers the "tick" event listener.
+       var fnStartAnimation = () => {
+         createjs.Ticker.setFPS(lib.properties.fps);
+         createjs.Ticker.addEventListener("tick", this.stage);
+       }
+       //Code to support hidpi screens and responsive scaling.
 
-            function resizeCanvas() {
-                var w = lib.properties.width,
-                    h = lib.properties.height;
-                var iw = window.innerWidth,
-                    ih = window.innerHeight;
-                var pRatio = window.devicePixelRatio || 1,
-                    xRatio = iw / w,
-                    yRatio = ih / h,
-                    sRatio = 1;
-                if (isResp) {
-                    if ((respDim == 'width' && lastW == iw) || (respDim == 'height' && lastH == ih)) {
-                        sRatio = lastS;
-                    } else if (!isScale) {
-                        if (iw < w || ih < h)
-                            sRatio = Math.min(xRatio, yRatio);
-                    } else if (scaleType == 1) {
-                        sRatio = Math.min(xRatio, yRatio);
-                    } else if (scaleType == 2) {
-                        sRatio = Math.max(xRatio, yRatio);
-                    }
-                }
-                this.canvas.width = w * pRatio * sRatio;
-                this.canvas.height = h * pRatio * sRatio;
-                this.canvas.style.width = dom_overlay_container.style.width = anim_container.style.width = w * sRatio + 'px';
-                this.canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h * sRatio + 'px';
-                stage.scaleX = pRatio * sRatio;
-                stage.scaleY = pRatio * sRatio;
-                lastW = iw;
-                lastH = ih;
-                lastS = sRatio;
-            }
-        }
-    }
-}
+       this.makeResponsive(true, 'both', false, 1);
+       fnStartAnimation();
+     },
+     makeResponsive(isResp, respDim, isScale, scaleType) {
+       var lastW, lastH, lastS = 1;
+       window.addEventListener('resize', this.resizeCanvas);
+
+       function resizeCanvas() {
+         var w = lib.properties.width,
+           h = lib.properties.height;
+         var iw = window.innerWidth,
+           ih = window.innerHeight;
+         var pRatio = window.devicePixelRatio || 1,
+           xRatio = iw / w,
+           yRatio = ih / h,
+           sRatio = 1;
+         if (isResp) {
+           if ((respDim == 'width' && lastW == iw) || (respDim == 'height' && lastH == ih)) {
+             sRatio = lastS;
+           } else if (!isScale) {
+             if (iw < w || ih < h)
+               sRatio = Math.min(xRatio, yRatio);
+           } else if (scaleType == 1) {
+             sRatio = Math.min(xRatio, yRatio);
+           } else if (scaleType == 2) {
+             sRatio = Math.max(xRatio, yRatio);
+           }
+         }
+         this.canvas.width = w * pRatio * sRatio;
+         this.canvas.height = h * pRatio * sRatio;
+         this.canvas.style.width = dom_overlay_container.style.width = anim_container.style.width = w * sRatio + 'px';
+         this.canvas.style.height = anim_container.style.height = dom_overlay_container.style.height = h * sRatio + 'px';
+         stage.scaleX = pRatio * sRatio;
+         stage.scaleY = pRatio * sRatio;
+         lastW = iw;
+         lastH = ih;
+         lastS = sRatio;
+       }
+     }
+   }
+ }
