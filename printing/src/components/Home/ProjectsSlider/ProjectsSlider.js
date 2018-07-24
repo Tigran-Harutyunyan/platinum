c5acac7d365570f8d185a33739bb9869490d651b
Tigran Harutyunyan <tigran.harutyunyan@accontech.com>
2018-07-24 11:53:08 +0400

    projects slider component

--- printing/src/components/Home/ProjectsSlider/ProjectsSlider.js
+++ printing/src/components/Home/ProjectsSlider/ProjectsSlider.js
@@ -10,16 +10,26 @@ export default {
         slidesPerView: 5,
         spaceBetween: 20,
         autoplay: true,
+        isInitialized: false,
         //loop: true,
         navigation: {
           nextEl: '.swiper-button-next',
           prevEl: '.swiper-button-prev',
         },
-        /*  breakpoints: {
-           1250: {
-             slidesPerView: 4,
-           }
-         } */
+        breakpoints: {
+          1250: {
+            slidesPerView: 4
+          },
+          860: {
+            slidesPerView: 3
+          },
+          560: {
+            slidesPerView: 2
+          },
+          340: {
+            slidesPerView: 1
+          }
+        }
       }
     }
   },
@@ -27,21 +37,33 @@ export default {
     swiper() {
       return this.$refs.myProjects.swiper
     },
+    projectsSliderImages: {
+      get: function () {
+        return this.$store.getters.projectsSliderImages;
+      },
+      set: function () {}
+    }
   },
   created() {
-    this.projects = data.graphic;
+    let data = this.$store.getters.projectsSliderImages;
+    if (!data.length) {
+      this.$store.dispatch('getProjectSliderImages');
+    }
   },
-  mounted() {
-    $('.open-popup-link').magnificPopup({
-      type: 'image',
-      gallery: {
-        enabled: true
-      },
-      mainClass: 'mfp-with-zoom',
-      zoom: {
-        enabled: true,
-        duration: 300
-      }
-    });
+  updated() {
+    if (!this.isInitialized) {
+      this.isInitialized = true;
+      $('.open-popup-link').magnificPopup({
+        type: 'image',
+        gallery: {
+          enabled: true
+        },
+        mainClass: 'mfp-with-zoom',
+        zoom: {
+          enabled: true,
+          duration: 300
+        }
+      });
+    }
   }
 }
