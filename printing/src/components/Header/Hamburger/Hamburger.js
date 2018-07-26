5299d4a402d2ef11af12fbc28e89ec1eb0296a0a
Tigran Harutyunyan <tigran.harutyunyan@accontech.com>
2018-07-26 13:28:43 +0400

    better scroll usability

--- printing/src/components/Header/Hamburger/Hamburger.js
+++ printing/src/components/Header/Hamburger/Hamburger.js
@@ -1,45 +1,57 @@
 export default {
   data() {
-    return { 
-        isActive: false
+    return {
+      isActive: false
     }
-  }, 
+  },
   methods: {
     goTo(section) {
-        let param = '';
-        let customOffset = 0;
-        switch (section) {
-          case 'services':
-            param = '#section-services';
-            break;
-          case 'about-us':
-            param = '#section-about-us';
-            break;
-          case 'projects':
-            param = '#section-portfolio';
-            break;
-          case 'why':
-            param = '#reasons-section';
-            break;
-          case 'staff':
-            param = '#staff-section';
-            customOffset = 50;
-            break;
-          case 'contact-us':
-            param = '#section-5';
-            customOffset = -140;
-            break;
-          default:
-            param = '';
-        }
-        this.$router.push({
-          name: 'Home'
-        });
-        //this.$router.push({ name: 'Home', params: { section: section } });
-        this.isHamburgerActive = false;
+      let param = '';
+      let customOffset = 0;
+      switch (section) {
+        case 'services':
+          customOffset = 20;
+          param = '#section-services';
+          break;
+        case 'about-us':
+          customOffset = 30;
+          param = '#section-about-us';
+          break;
+        case 'projects':
+          customOffset = 20;
+          param = '#section-portfolio';
+          break;
+        case 'why':
+          customOffset = 20;
+          param = '#reasons-section';
+          break;
+        case 'staff':
+          param = '#staff-section';
+          customOffset = 190;
+          break;
+        case 'contact-us':
+          param = '#section-5';
+          customOffset = -140;
+          break;
+        default:
+          param = '';
+      }
+      this.isActive = false;
+
+      if (this.$route.name === 'Home') {
         $('html, body').animate({
           scrollTop: !param ? 0 : $(param).offset().top - customOffset
         }, 0);
-      },
+      } else {
+        let params = {
+          param: param,
+          offset: customOffset
+        }
+        this.$store.dispatch('setScrollParams', params );
+        this.$router.push({
+          name: 'Home'
+        });
+      }
+    }
   }
 }
