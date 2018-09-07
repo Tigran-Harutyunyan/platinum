61a16f4cd89afbfe0a553ca024dd88185ba87ac8
Tirgan Harutryunyan <tigran.harutyunyan@accontech.com>
2018-09-07 17:04:41 +0400

    ads

--- printing/src/components/Advertisement/Advertisement.js
+++ printing/src/components/Advertisement/Advertisement.js
@@ -1,51 +1,107 @@
+import CarouselPopup from '../../commonComponents/CarouselPopup/CarouselPopup.vue';
 export default {
   data() {
     return {
-      swiperOption: {
-        slidesPerView: 7,
-        spaceBetween: 20,
-        autoplay: true,
-        isInitialized: false,
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-          1250: {
-            slidesPerView: 4
-          },
-          860: {
-            slidesPerView: 3
-          },
-          560: {
-            slidesPerView: 2
-          },
-          340: {
-            slidesPerView: 1
+      index: 0,
+      dialogTableVisible: false,
+      audioADS: [],
+      adCategories: {},
+      category : {}
+    }
+  },
+  components: {
+    CarouselPopup
+  },
+  watch: {
+    advertisements:{
+      handler : function(ads){ 
+        for (const key in ads) {
+          if (ads.hasOwnProperty(key)) { 
+            if (key == 'audio') {
+              this.audioADS.push(ads[key]);
+              delete ads[key];
+              break;
+            } 
           }
         }
+        this.adCategories = ads;
       }
     }
   },
   computed: {
-    swiper() {
-      return this.$refs.myProjects.swiper;
-    },
-    projectsSliderImages: {
+    advertisements: {
       get: function () {
-        return this.$store.getters.projectsSliderImages;
+        return this.$store.getters.getAdvertisements;
       },
       set: function () {}
-    } 
-  },
-  methods: {
-   
+    }
   },
   created() {
-    let data = this.$store.getters.projectsSliderImages;
-    if (!data.length) {
-      this.$store.dispatch('getProjectSliderImages');
+    this.$store.dispatch('getAdvertisements');
+  },
+  methods: {
+    openPopup(index, category) {
+      this.index = index;
+      this.category  = category;
+      this.dialogTableVisible = true;
     }
-  } 
-} 
+  },
+  directives: {
+    carousel: {
+      inserted: function (el, binding) { 
+        let slidesToShow = binding.value < 7 ? binding.value : 7;
+        $(el).slick({
+          autoplay: true,
+          dots: false,
+          infinite: true,
+          speed: 300,
+          slidesToShow: slidesToShow,
+          slidesToScroll: 1,
+          draggable: false,
+          responsive: [{
+              breakpoint: 1640,
+              settings: {
+                infinite: true,
+                slidesToShow: 6,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 1350,
+              settings: {
+                infinite: true,
+                slidesToShow: 5,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 1090,
+              settings: {
+                infinite: true,
+                slidesToShow: 4,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 830,
+              settings: {
+                infinite: true,
+                slidesToShow: 3,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 560,
+              settings: {
+                infinite: true,
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                draggable: true
+              }
+            }
+          ]
+        });
+      },
+    }
+  },
+}
