b9ab666b7ab76b3a769f928ffc738125488498e5
megasharks <tigran.accontech@gmail.com>
2018-09-09 23:43:46 +0400

    changed the login

--- printing/src/components/Login/Login.js
+++ printing/src/components/Login/Login.js
@@ -7,28 +7,35 @@ import {
 import {
   EventBus
 } from '../../event-bus.js';
+import Preloader from '../../commonComponents/Preloader/Preloader.vue';
 export default {
   data() {
     return {
       email: "tigran@mailinator.com",
       password: "tigran",
-      loading: false
+      isLoading: false,
+      dialogTableVisible: true,
+      recoveryMail:'',
+      loginMode: true
     }
   },
   computed: {
     isAllowedToRequest() {
-      return !this.$v.email.$invalid && !this.$v.password.$invalid && !this.loading;
+      return !this.$v.email.$invalid && !this.$v.password.$invalid && !this.isLoading;
     }
+  },
+  components: {
+    Preloader
   },
   methods: {
     onLogin() {
       if (this.isAllowedToRequest) {
-        this.loading = true;
+        this.isLoading = true;
         this.$store.dispatch('login', {
           email: this.email,
           password: this.password
         }).then((response) => {
-          this.loading = false;
+          this.isLoading = false;
           if (response.error) {
             this.$notify({
               title: 'Login',
@@ -40,17 +47,14 @@ export default {
           if (response.success) {
             this.$emit('LoginSuccess',response)
             let storage = localStorage.getItem('platinumInk') ? JSON.parse(localStorage.getItem("platinumInk")) : {};
-            storage.user = response;
-            this.user = response;
+            storage.user = response; 
             localStorage.setItem('platinumInk', JSON.stringify(storage));
-            this.$store.dispatch('setStorage', storage);
-            this.showLoginDropdown = false;
-            this.loginMode = true;
-            this.isAuthenticated = true;
+            this.$store.dispatch('setStorage', storage);  
+            this.close();
           }
           EventBus.$emit('authChanged');
         }).catch((error) => {
-          this.loading = false;
+          this.isLoading = false;
           this.$notify({
             title: 'Login',
             message: error ? error : 'Failed to login',
@@ -59,6 +63,29 @@ export default {
           });
         });
       }
+    },
+    onPassRecovery() {
+      if (!this.$v.recoveryMail.$invalid) {
+        this.$store.dispatch('recover', {
+          email: this.recoveryMail
+        }).then((response) => {
+          this.isLoading = false;
+          if (response.success) {
+            this.$notify({
+              title: 'Password recovery',
+              message:  "Password recovery success",
+              position: "top-right",
+              type: "success"
+            });
+          }
+        }).catch((error) => {
+          this.isLoading = false;
+        });
+      }
+    },
+    close(){
+      this.$emit("close");
+      this.dialogTableVisible = false;
     }
   },
   validations: {
@@ -68,6 +95,10 @@ export default {
     }, 
     password: {
       required
+    },
+    recoveryMail: {
+      required,
+      email
     }
   }
 }
