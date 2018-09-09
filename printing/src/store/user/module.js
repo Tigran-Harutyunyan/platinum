559e32e5941008e64431e1346845b535173e06e7
megasharks <tigran.accontech@gmail.com>
2018-09-09 15:51:55 +0400

    user module

---
+++ printing/src/store/user/module.js
@@ -0,0 +1,11 @@
+import mutations from './userMutations';
+import getters from './userGetters';
+import actions from './userActions';
+import {state}   from './userState'; 
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+} 
