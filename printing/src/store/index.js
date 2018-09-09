b053cc51dd042243830c352f76b5e44f480ab0a8
megasharks <tigran.accontech@gmail.com>
2018-09-09 15:52:28 +0400

    store

--- printing/src/store/index.js
+++ printing/src/store/index.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'; 
 import module from './modules/module';
+import user from './user/module';
 Vue.use(Vuex)
 export default new Vuex.Store({
   modules: {
-    module
+    module,
+    user
   },
 }) 
