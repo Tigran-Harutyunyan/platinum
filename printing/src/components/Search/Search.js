53cb96b83983c4bacdead8b84fd8511278656be1
megasharks <tigran.accontech@gmail.com>
2018-09-09 23:42:46 +0400

    changed the search

--- printing/src/components/Search/Search.js
+++ printing/src/components/Search/Search.js
@@ -1,62 +1,47 @@
 var debounce = require('debounce');
+import ClickOutside from 'vue-click-outside'
 export default {
   data() {
-    return {search_key: '', searchResults: '', showSearchResults: false}
+    return {
+      searchKey: '',
+      searchResults: '',
+      showSearchResults: false
+    }
   },
-  computed : {
+  computed: {
     noResults() {
       return this.searchResults.length == 0
     }
   },
-  methods : {
-    closeMe() {
-      setTimeout(() => {
-        this.showSearchResults = false;
-      }, 150);
+  watch:{
+    searchKey(val){
+      this.search();
+    }
+  },
+  directives: {
+    ClickOutside
+  },
+  methods: {
+    close() {
+      this.showSearchResults = false;
+      this.searchKey = '';
     },
-    checkDropdown(closeIt) {
-      if (closeIt === true) {
-        setTimeout(() => {
-          this.showSearchResults = false;
-        }, 150);
-      } else {
-        if (this.search_key.length) {
-          this.showSearchResults = true;
-        }
-      }
-    },
-    onSearch: debounce(function (e) {
-      if (this.search_key.trim().length === 0) {
+    search: debounce(function (e) {
+      if (this.searchKey.trim().length === 0) {
         this.showSearchResults = false;
         this.searchResults = [];
       } else {
         this
           .$store
-          .dispatch('getSearchResults', {search: this.search_key})
+          .dispatch('getSearchResults', {
+            search: this.searchKey
+          })
           .then((response) => {
-            let results = [];
-            if (response.categories && Array.isArray(response.categories)) {
-              response
-                .categories
-                .forEach(element => {
-                  element.url = `/category/${element.id}`;
-                  results.push(element)
-                });
-            }
-            if (response.products && Array.isArray(response.products)) {
-              response
-                .products
-                .forEach(element => {
-                  element.url = `/product/${element.id}`;
-                  results.push(element)
-                });
-            }
             this.showSearchResults = true;
-            this.searchResults = results;
-
+            this.searchResults = response;
           })
           .catch((error) => {});
       }
-    }, 400)
+    }, 300)
   }
 }
