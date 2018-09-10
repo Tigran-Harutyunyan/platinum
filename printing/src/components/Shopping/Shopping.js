bdbc4c89bb79da6d5a85ce3663e4c6657b30efc1
megasharks <tigran.accontech@gmail.com>
2018-09-11 01:38:55 +0400

    shopping list

--- printing/src/components/Shopping/Shopping.js
+++ printing/src/components/Shopping/Shopping.js
@@ -4,6 +4,7 @@ import Slider from "../Slider/Slider.vue";
 import Sidebar from "../Sidebar/Sidebar.vue";
 import HelperLinks from "../HelperLinks/HelperLinks.vue";
 import InfoTabs from "../InfoTabs/InfoTabs.vue";
+import HeaderCategories from '../MobileProductList/MobileProductList.vue'; 
 export default {
   name: 'home',
   components: {
@@ -12,23 +13,80 @@ export default {
     Footer,
     Sidebar,
     HelperLinks,
-    InfoTabs
+    InfoTabs,
+    HeaderCategories
   },
   data() {
     return {
-      showSidebar: false
+      showSidebar: false,
+      productList: {},
     };
   },
   watch: {
     $route(to, from) {
       this.checkRoute(to.name);
+    },
+    products: function (newVal, oldVal) {
+      let products = newVal;
+      for (const key in products) {
+        if (products.hasOwnProperty(key)) {
+          const element = products[key];
+          element[0].categoryName = this.getCategoryName(element[0].category_id); 
+        }
+      }
+      this.isLoading = false;
+      this.productList = products; 
+      this.$store.dispatch('setSideBarProducts',products);
+    },
+    categories: function (newVal, oldVal) {
+      this.categories = newVal;
+      this.$store.dispatch('getProducts');
     }
   },
+  computed: {
+    products() {
+      return this.$store.getters.products;
+    },
+    categories: {
+      get: function () {
+        return this.$store.getters.categories;
+      },
+      set: function () {}
+    },
+    sidebarProducts: {
+      get: function () {
+        return this.$store.getters.sidebarProducts;
+      },
+      set: function () {}
+    }
+  }, 
   methods: {
     checkRoute(route) {
       this.showSidebar =
         route == "Categories" || route == "ProductDetail" ? true : false;
+    },
+    getCategoryName(category_id) {
+      let categoryName = "";
+      this.categories.forEach(category => {
+        if (category_id == category.id) {
+          categoryName = category.name;
+        }
+      });
+      return categoryName;
     }
+  }, 
+  mounted(){
+    if (this.sidebarProducts) { 
+      this.isLoading = false;
+      this.productList = this.sidebarProducts;
+    } else {
+      let categories = this.$store.getters.categories;
+      if (!categories) {
+        this.$store.dispatch('getCategories');
+      } else {
+        this.$store.dispatch('getProducts');
+      }
+    }  
   },
   created() {
     this.checkRoute(this.$route.name);
--- printing/src/components/Sidebar/Sidebar.js
+++ printing/src/components/Sidebar/Sidebar.js
@@ -1,71 +1,20 @@
 import Preloader from '../../commonComponents/Preloader/Preloader.vue';
 export default {
   data() {
-    return {
-      productList: {},
+    return { 
       isLoading: true
     }
+  },
+  props: {
+    productList: {}
   },
   components:{
     Preloader
   },
   watch: {
-    products: function (newVal, oldVal) {
-      let products = newVal;
-      for (const key in products) {
-        if (products.hasOwnProperty(key)) {
-          const element = products[key];
-          element[0].categoryName = this.getCategoryName(element[0].category_id); 
-        }
-      }
+    productList(newVal) { 
       this.isLoading = false;
-      this.productList = products;
-      this.$store.dispatch('setSideBarProducts',products);
-    },
-    categories: function (newVal, oldVal) {
-      this.categories = newVal;
-      this.$store.dispatch('getProducts');
-    }
-  },
-  computed: {
-    products() {
-      return this.$store.getters.products;
-    },
-    categories: {
-      get: function () {
-        return this.$store.getters.categories;
-      },
-      set: function () {}
-    },
-    sidebarProducts: {
-      get: function () {
-        return this.$store.getters.sidebarProducts;
-      },
-      set: function () {}
-    }
-  },
-  methods: { 
-    getCategoryName(category_id) {
-      let categoryName = "";
-      this.categories.forEach(category => {
-        if (category_id == category.id) {
-          categoryName = category.name;
-        }
-      });
-      return categoryName;
-    }
-  },
-  mounted() { 
-    if (this.sidebarProducts) { 
-      this.isLoading = false;
-      this.productList = this.sidebarProducts;
-    } else {
-      let categories = this.$store.getters.categories;
-      if (!categories) {
-        this.$store.dispatch('getCategories');
-      } else {
-        this.$store.dispatch('getProducts');
-      }
+      this.productList = newVal; 
     } 
-  }
+  }   
 }
