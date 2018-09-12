5545ec126b317d84adc02a49c3cff51359cbadd2
megasharks <tigran.accontech@gmail.com>
2018-09-13 01:13:49 +0400

    responsive popup

--- printing/src/components/Orders/OrderDetailsPopup/OrderDetailsPopup.js
+++ printing/src/components/Orders/OrderDetailsPopup/OrderDetailsPopup.js
@@ -1,10 +1,18 @@
  export default { 
+   computed:{
+    isVisible:{
+      get: function () {
+        return this.visible;
+      },
+      set: function () {} 
+    }
+   },
    props: {
      order:{
          type: Object
      },
      visible: {
-         type:Boolean
+         type: Boolean
      }
    } 
  }
