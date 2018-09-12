602703810e9b84190004c8aa442aaddaeeefb2bb
megasharks <tigran.accontech@gmail.com>
2018-09-13 01:13:37 +0400

    utils function

--- printing/src/utils.js
+++ printing/src/utils.js
@@ -1,5 +1,5 @@
  const utils = {
-   getLang(params) {
+    getLang(params) {
      let storage = localStorage.getItem('platinumInk') ? JSON.parse(localStorage.getItem("platinumInk")) : {};
      if (typeof storage.locale == 'undefined') {
        storage.locale = 'en'
@@ -7,12 +7,16 @@
      return storage.locale;
    },
    getSortId(key, propertyNames) {
-    for (const _key in propertyNames) {
-      if (propertyNames.hasOwnProperty(_key) && key === propertyNames[_key].name) {
-        return parseInt(propertyNames[_key].order_id);
-      }
-    }
-  },
+     for (const _key in propertyNames) {
+       if (propertyNames.hasOwnProperty(_key) && key === propertyNames[_key].name) {
+         return parseInt(propertyNames[_key].order_id);
+       }
+     }
+   },
+   getUserToken() {
+     let storage = localStorage.getItem("platinumInk") ? JSON.parse(localStorage.getItem("platinumInk")) : {}; 
+     return storage && storage.user && storage.user.token.length>0 ? storage.user.token : null;
+   }
  };
 
  export default utils;
