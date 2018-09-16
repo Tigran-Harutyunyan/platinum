24e8456e9b62243ddb2afc694c5bb657242355a6
megasharks <tigran.accontech@gmail.com>
2018-09-16 10:41:39 +0400

    refactor

--- printing/src/components/Header/Header.js
+++ printing/src/components/Header/Header.js
@@ -4,36 +4,32 @@ import {
 import Login from '../Login/Login.vue';
 import Search from '../Search/Search.vue';
 import SearchMobile from '../SearchMobile/SearchMobile.vue';
-import Hamburger from './Hamburger/Hamburger.vue'; 
-import LanguageSwitcher  from './LanguageSwitcher/LanguageSwitcher.vue';
-export default { 
+import Hamburger from './Hamburger/Hamburger.vue';
+import LanguageSwitcher from './LanguageSwitcher/LanguageSwitcher.vue';
+export default {
   data() {
-    return { 
-      loading: false, 
-      recoveryMail: "",
-      isAuthenticated: false, 
-      user: {},
-      currentRoute: "", 
+    return {
       showLogin: false,
-      showPasswordRecovery: false,
       isMobileSearch: false
     }
   },
-  components:{
+  components: {
     Login,
     Search,
     SearchMobile,
-    Hamburger, 
+    Hamburger,
     LanguageSwitcher
   },
-  watch: {
-    '$route' (to, from) {
-      this.currentRoute = to.name;
-    }
-  },
-  computed:{
-    storage(){
-      return this.$store.getters.storage;
+
+  computed: {
+    user: {
+      get: function () {
+        return this.$store.getters.getUser;
+      },
+      set: function () {}
+    },
+    isAuthenticated() {
+      return this.$store.getters.getToken;
     },
     cartItems: {
       get: function () {
@@ -43,85 +39,44 @@ export default {
     },
   },
   methods: {
+
     logout() {
-      this.checkAuth();
-      if (this.user) {
-        let formData = new FormData();
-        formData.append('token', this.user ? this.user.token : "");
-        this.$store.dispatch('logout', {
-          formData
-        }).then((response) => {
-          this.removeUser();
-        }).catch((error) => {
-          this.removeUser();
-        });
-      } else {
+      this.$store.dispatch('logout').then((response) => {
         this.removeUser();
-      }
+      }).catch((error) => {
+        this.removeUser();
+      });
     },
 
     removeUser() {
-      let storage = localStorage.getItem('platinumInk') ? JSON.parse(localStorage.getItem("platinumInk")) : {};
-      delete storage.user;
-      localStorage.setItem('platinumInk', JSON.stringify(storage));
-      this.$store.dispatch('setStorage', storage);
-      this.isAuthenticated = false;
       EventBus.$emit('onLogout');
-      EventBus.$emit('authChanged'); 
-      if(this.$route.name !== 'Home',this.$route.name !== 'ProductDetail'){
+      EventBus.$emit('authChanged');
+      if (this.$route.name !== 'Home', this.$route.name !== 'ProductDetail') {
         this.$router.push({
           name: 'Categories',
           params: {
             id: 1
           }
         });
-      } 
+      }
     },
 
     toSignupPage() {
       this.$router.push({
         name: 'SignUp'
       });
-      this.showLoginDropdown = false;
-      this.loginMode = true;
     },
 
-    hideLoginDropdown() {
-      this.showLoginDropdown = false;
-      this.loginMode = true
-    },
- 
-    toggleLang(locale) { 
-      let storage = localStorage.getItem('platinumInk') ? JSON.parse(localStorage.getItem("platinumInk")) : {};
-      if (locale != storage.locale) {
-        storage.locale = locale;
-        localStorage.setItem('platinumInk', JSON.stringify(storage));
-        this.$store.dispatch('setStorage', storage);
-        location.reload();
-      }
-    }, 
-    
     onLoginSuccess(response) {
-      let storage = localStorage.getItem('platinumInk') ? JSON.parse(localStorage.getItem("platinumInk")) : {};
-      storage.user = response;
-      this.user = response;
-      localStorage.setItem('platinumInk', JSON.stringify(storage));
-      this.$store.dispatch('setStorage', storage);
-      this.showLoginDropdown = false;
-      this.loginMode = true;
-      this.isAuthenticated = true;
+      this.$store.dispatch('getBasketProducts'); 
     },
-    
-    checkAuth() {
-       this.user = this.storage.user ? this.storage.user : null;
-    }
   },
-  mounted() { 
-    this.user = this.storage.user;
-    this.isAuthenticated = this.storage.user;
-    this.currentRoute = this.$route.name;
+  mounted() {
     EventBus.$on('logout', () => {
-      this.removeUser();
+      this.logout();
     });
-  } 
+    EventBus.$on('showLogin', () => {
+      this.showLogin = true;
+    });
+  }
 }
