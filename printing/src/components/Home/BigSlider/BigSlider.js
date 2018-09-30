25198ee0d14419339ed040f72fe12346dd1dafc2
megasharks <tigran.accontech@gmail.com>
2018-09-30 11:21:17 +0400

    replaced main slider

--- printing/src/components/Home/BigSlider/BigSlider.js
+++ printing/src/components/Home/BigSlider/BigSlider.js
@@ -1,26 +1,36 @@
  import LogoAnimation from './LogoAnimation/LogoAnimation.vue';
  import SocialMedia from "../SocialMedia/SocialMedia.vue";
+ import {
+   EventBus
+ } from '../../../event-bus.js';
  export default {
    data() {
      return {
        currentSlide: 1,
-       slidesLength: '',
-       swiperOption: {
-         slidesPerView: 1,
-         fadeEffect: {
-           crossFade: true
-         },
-         speed: 2000,
-         effect: 'fade',
-         autoplay: {
-          delay: 5000,
-          },
-         pagination: {
-           el: '.big-slider-pagination',
-           clickable: true
-         }
-       },
+       slidesLength: '', 
        showBoxes: false
+     }
+   },
+   directives: {
+     carousel: {
+       inserted: function (el) {
+         $(el).slick({
+           autoplay: true,
+           dots: false,
+           infinite: true,
+           speed: 800,
+           autoplaySpeed: 4000,
+           dots: true,
+           fade: true,
+           cssEase: 'linear',
+           slidesToShow: 1,
+           slidesToScroll: 1,
+           draggable: false,
+         });
+         $(el).on('afterChange', (event, slick, currentSlide) => {
+           EventBus.$emit('slideChange', currentSlide + 1);
+         });
+       },
      }
    },
    components: {
@@ -28,28 +38,20 @@
      'social-media': SocialMedia
    },
    mounted() {
-     this.setSlidesItems(1); //set slide item info.
-
-     this.swiper.on('slideChange', () => {
-       //handle slide change.
-       let currentIndex = this.swiper.activeIndex + 1;
-       if (currentIndex === 2) {
-         this.showBoxes = true
+     EventBus.$on('slideChange', (currentSlide) => {
+       if (currentSlide === 2) {
+         this.showBoxes = true;
        }
-       this.setSlidesItems(currentIndex);
+       this.setSlidesItems(currentSlide);
      });
+     this.setSlidesItems(1); //set slide item info. 
+   },
 
-   },
-   computed: {
-     swiper() {
-       return this.$refs.bigSlider.swiper
-     }
-   },
    methods: {
      setSlidesItems(currentIndex) {
-       let slidesLength = this.swiper.slides.length;
+       let slidesLength = 2;
        this.slidesLength = (currentIndex < 10) ? '/0' + slidesLength : '/' + slidesLength;
        this.currentSlide = (currentIndex < 10) ? '0' + currentIndex : currentIndex;
      }
    }
- }
+ };
